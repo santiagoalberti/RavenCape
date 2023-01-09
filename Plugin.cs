9068b08894f4e68b327bb04081e58adbcf9cb08b
Goldenrevolver <22944589+Goldenrevolver@users.noreply.github.com>
2023-01-09 12:36:33 +0100

    Fixed item manager config bug when not playing in english by hardcoding default section name

--- Plugin.cs
+++ Plugin.cs
@@ -43,7 +43,7 @@ namespace RavenCape
                 "If on, the configuration is locked and can be changed by server admins only.");
             _ = ConfigSync.AddLockingConfigEntry(_serverConfigLocked);
 
-            Item tqRavenCape = new("raven_cape_bundle", "ravenCape_tq");  // If your folder name is "assets" like the default. You would use this syntax.
+            Item tqRavenCape = new("raven_cape_bundle", "ravenCape_tq", "assets", "Raven Cape");  // If your folder name is "assets" like the default. You would use this syntax.
             // Item ironFangAxe = new("ironfang", "IronFangAxe", "IronFang"); // If your asset is in a custom folder named IronFang and not the default "assets" folder. You would use this syntax.
 
             tqRavenCape.Name.English("Raven Cape"); // You can use this to fix the display name in code
@@ -56,7 +56,7 @@ namespace RavenCape
             tqRavenCape.RequiredUpgradeItems.Add("WolfHairBundle", 5); // 10 Silver: You need 10 silver for level 2, 20 silver for level 3, 30 silver for level 4
             tqRavenCape.CraftAmount = 1; // We really want to dual wield these
 
-            Item tqImbuedRavenCape = new("raven_cape_bundle", "ravenCapeFeather_tq");  // If your folder name is "assets" like the default. You would use this syntax.
+            Item tqImbuedRavenCape = new("raven_cape_bundle", "ravenCapeFeather_tq", "assets", "Raven Cape");  // If your folder name is "assets" like the default. You would use this syntax.
             // Item ironFangAxe = new("ironfang", "IronFangAxe", "IronFang"); // If your asset is in a custom folder named IronFang and not the default "assets" folder. You would use this syntax.
 
             tqImbuedRavenCape.Name.English("Imbued Raven Cape"); // You can use this to fix the display name in code
